707b29d933eb5a0ab69518e6fde7dc1aecab6ae6
chadkeeven <chadkeeven@gmail.com>
2017-12-20 07:50:02 -0700

    added comments

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+//link to movie file to use 'get function'
 const movie = require("./movie");
+//Array of movies to search for
 var threeFavoriteMovies = ["Alien", "Logan", "Star Wars"];
+//runs through threeFavoriteMovies array
+//calls the get function in the 'movie.js' file 
+//for each element(film) in the array
 threeFavoriteMovies.forEach(function(film){
-  movie(film);
-  
+	movie(film);
+
 });
--- movie.js
+++ movie.js
@@ -1,15 +1,15 @@
+//link outside files to movie.js
 const request = require('request');
 const env = require('./env');
 
-
+//function that takes in one argument 'movieTitle'
+//makes a api request to a custom google search 
+//returns a title for each argument passed 
 function get(movieTitle) {
-	var searchRequest = request('https://www.googleapis.com/customsearch/v1?key=' + env.apiKey + '&cx=' + env.apiId +'&q=' + movieTitle, function(error, response, body){
-		//console.log('error:', error);
-		//console.log('statusCode:', response & response.statusCode);
-		var bodyParsed = JSON.parse(body);
-		console.log(bodyParsed.items[0].title);
+	var searchRequest = request('https://www.googleapis.com/customsearch/v1?key=' + env.apiKey + '&cx=' + env.apiId + '&q=' + movieTitle, function(error, response, body) {
+		var bodyParsed = JSON.parse(body); //parse body HTML into JSON
+		console.log(bodyParsed.items[0].title); //display results to console
 	});
 }
-//get();
-
+//allows the get function to be used in other files
 module.exports = get;
